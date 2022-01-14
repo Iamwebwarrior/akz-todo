0dbd828620e47b1ae8203a5840f877ba166e0f40
iamwebwarrior <iamwebwarrior@gmail.com>
2022-01-14 13:18:44 +0530

    my first commit

--- FRONTEND/config.js
+++ FRONTEND/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "https://akz-todo-app.herokuapp.com/"
+    baseURL : "https://akz-todo-app.herokuapp.com/api"
 })
